5edb819610e1f6e40f13db806b5a55cbb2a37290
EPAXGAMING <sureyjan@gmail.com>
2025-05-04 16:38:48 +0200

    update no .bat scripts

--- scripts/run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-qemu-system-riscv64.exe -M virt -cpu rv64 -device ramfb -device qemu-xhci -device usb-kbd -device usb-mouse -drive if=pflash,unit=0,format=raw,file=ovmf\ovmf-code-riscv64.fd,readonly=on -cdrom os.iso
-pause
